9108a781a8d33a0fc4787df72f14dded92e1f07f
RaghadQiari <157514712+RaghadQiari@users.noreply.github.com>
2024-02-14 19:29:48 +0300

    Update GROUP4.h

--- GROUP4.h
+++ GROUP4.h
@@ -1,1 +1,21 @@
+#pragma once
+#ifndef GROUP4_H
+#define GROUP4_H
+#include <iostream>
+#include "sqlConn.h" 
+using namespace std;
+
+class User : private OrderProduct, private OrderProject, private Customer {
+	int Customer_ID;
+	int Product_ID;
+	int Project_ID;
+	double amount_paid;
+
+public:
+	void Services(sql::Connection* con);
+
+};
+
+
+#endif // GROUP4_H
 
